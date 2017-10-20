da97abfdddcbf34c598becb51390adac7f952983
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-10-20 14:07:44 +0200

    ES6 syntax

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-export const isReactComponent = require('./util/is-react-component');
-export const RenderCounter = require('./wrappers/render-counter');
-export const Lorem = require('./components/react-lorem');
-export const VCenter = require('./components/layout/react-vcenter');
+import _isReactComponent from './util/is-react-component';
+export { _isReactComponent as isReactComponent };
+
+import _RenderCounter from './wrappers/render-counter';
+export { _RenderCounter as RenderCounter };
+
+import _Lorem from './components/react-lorem';
+export { _Lorem as Lorem };
+import _VCenter from './components/layout/react-vcenter';
+export { _VCenter as VCenter };
+import _AboveFold from './components/layout/react-above-fold';
+export { _AboveFold as AboveFold };
