5a07fcc6a1e3318400c81bbff734e02ee1a9c618
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2019-03-04 16:25:43 +0100

    Add editorconfig file & eslint fixes

--- src/components/sep.js
+++ src/components/sep.js
@@ -2,20 +2,19 @@ import React from 'react'; //eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 
 class Sep extends React.Component {
-    static propTypes = {
-        width: PropTypes.string, //default '0.3em'
-    };
-    
-    render() {
-        const props = this.props;
-        return (
-            <span style={{ display: 'inline-block', width: props.width || '0.3em' }}/>
-        );
-    }
-    
-    shouldComponentUpdate(nextProps) {
-        return this.props.width !== nextProps.width;
-    }
+	static propTypes = {
+		width: PropTypes.string, //default '0.3em'
+	};
+	
+	render() {
+		const props = this.props;
+		
+		return (<span style={{display: 'inline-block', width: props.width || '0.3em'}}/>); //eslint-disable-line react-native/no-inline-styles
+	}
+	
+	shouldComponentUpdate(nextProps) {
+		return this.props.width !== nextProps.width;
+	}
 }
 
 export default Sep;
